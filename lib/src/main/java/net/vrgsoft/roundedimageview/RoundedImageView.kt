637044c8ce0e0718f3e7413b4fed398053f252e6
Pavlo Nikitin <pawlo2102@gmail.com>
2020-05-07 14:39:12 +0300

    fixed bitmap creation size

--- lib/src/main/java/net/vrgsoft/roundedimageview/RoundedImageView.kt
+++ lib/src/main/java/net/vrgsoft/roundedimageview/RoundedImageView.kt
@@ -68,7 +68,7 @@ class RoundedImageView @JvmOverloads constructor(
 
     private fun invalidatePath(w: Int, h: Int) {
         path = getPath(w, h).also {
-            if (drawable != null && isAttachedToWindow) {
+            if (drawable != null && w > 0 && h > 0 && isLaidOut) {
                 maskBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
                 shaderCanvas.setBitmap(maskBitmap)
                 antiAliasPaint.shader = BitmapShader(
